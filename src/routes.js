fb2f1e4f79e717e8cc625f6ffdccabcca7d0a91a
Lucas <lukas-gabriel2002@hotmail.com>
2020-12-01 15:48:09 -0300

    primeiras configs

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', function (req, res) {});
+
+export default routes;
---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+import routes from './routes';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api', routes);
+
+app.listen(process.env.PORT || 8080, () => console.log('Server iniciado'));
