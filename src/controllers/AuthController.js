9efcfcb10d86c93fc241b904e017b6622bb46001
Lucas <lukas-gabriel2002@hotmail.com>
2020-12-03 14:11:21 -0300

    nem sei mais

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,65 @@
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User.js';
+
+dotenv.config();
+
+function generateToken(params = {}) {
+  return jwt.sign(params, process.env.APP_SECRET, {
+    expiresIn: 86400,
+  });
+}
+
 export default {
-  async register(req, res) {},
-  async login(req, res) {},
+  async register(req, res) {
+    const { username, password } = req.body;
+
+    try {
+      const result = await User.findOne({ where: { username } });
+
+      if (result) {
+        res.status(400).send({ error: 'Usuário já existe' });
+      }
+
+      const user = await User.create({ username, password });
+
+      user.password = undefined;
+
+      res.status(200).send({
+        user,
+        token: generateToken({ id: user.id }),
+      });
+    } catch (error) {
+      res.status(400).send({ error: 'Campos vazios' });
+    }
+  },
+
+  async login(req, res) {
+    const { username, password } = req.body;
+
+    try {
+      const user = await User.findOne({ where: { username } });
+
+      if (!user) {
+        res.status(400).send({ error: 'Dados inválidos' });
+      }
+
+      const userMatch = await bcrypt.compare(password, user.password);
+
+      if (!userMatch) {
+        res.status(400).send({ error: 'Dados inválidos' });
+      }
+
+      user.password = undefined;
+
+      res.status(200).send({
+        user,
+        token: generateToken({ id: user.id }),
+      });
+    } catch (error) {
+      res.status(400).send({ error: 'Campos vazios' });
+    }
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
 import AuthController from './controllers/AuthController.js';
+import authMiddleware from './middlewares/auth.js';
 
 const routes = express.Router();
 
 routes.post('/register', AuthController.register);
 routes.post('/login', AuthController.login);
+
+routes.use(authMiddleware);
 
 export default routes;
