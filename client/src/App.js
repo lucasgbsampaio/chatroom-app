e36847de1231f0208ac369bae8ef0306a9df7855
Lucas <lukas-gabriel2002@hotmail.com>
2020-12-09 20:42:03 -0300

    primeiras configs

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import './App.css';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import ChatRoom from './pages/ChatRoom';
 
 export default function App() {
-  return <div>Teste</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/chatroom" element={<ChatRoom />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
---
+++ client/src/services/api.js
@@ -0,0 +1,27 @@
+export const API_URL = '/api';
+
+export function USER_REGISTER(body) {
+  return {
+    url: API_URL + '/register',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export function USER_LOGIN(body) {
+  return {
+    url: API_URL + '/login',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
