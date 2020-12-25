1a9d5bca5322b76e7dc8cee5acd3b26df8c30d14
lucasfe4nor <lukasfeanorzr5123@hotmail.com>
2020-12-25 18:52:50 -0300

    deploy

---
+++ src/config/database.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  logging: false,
+  port: process.env.DB_PORT,
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+  use_env_variable: 'DATABASE_URL',
+};
