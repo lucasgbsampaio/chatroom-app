ee6a0102b9865900c9b11979ab5a31a31b63bc66
lucasfe4nor <lukasfeanorzr5123@hotmail.com>
2020-12-25 23:44:02 -0300

    u

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,10 +1,11 @@
-require('dotenv').config();
-
-console.log('a');
-
 module.exports = {
   production: {
     dialect: 'postgres',
     use_env_variable: 'DATABASE_URL',
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
   },
 };
