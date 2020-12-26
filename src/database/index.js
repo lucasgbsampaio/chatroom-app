62546676ae192cf0e464f1f93f26067b4875a97d
lucasfe4nor <lukasfeanorzr5123@hotmail.com>
2020-12-25 23:49:06 -0300

    a

--- src/database/index.js
+++ src/database/index.js
@@ -8,7 +8,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connection = new Sequelize(
-  'postgres://goperoiyoyjeyx:0abad3db668f8ffc3b7c2ae592029743093114b01961aace9b27da5e0cfa1423@ec2-100-25-231-126.compute-1.amazonaws.com:5432/dauqq62rfnbvam'
+  'postgres://goperoiyoyjeyx:0abad3db668f8ffc3b7c2ae592029743093114b01961aace9b27da5e0cfa1423@ec2-100-25-231-126.compute-1.amazonaws.com:5432/dauqq62rfnbvam',
+  {
+    dialect: 'postgres',
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  }
 );
 
 User.init(connection);
