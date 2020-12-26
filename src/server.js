b4336dfd8142d7b3955a4bf96e57325a041a0080
lucasfe4nor <lukasfeanorzr5123@hotmail.com>
2020-12-26 19:56:41 -0300

    finalizado

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://chat-pern.herokuapp.com/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
 });
