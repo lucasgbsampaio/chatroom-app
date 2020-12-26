4f35e0ab086c1f5b180ca7967dd00ca83c85fd84
lucasfe4nor <lukasfeanorzr5123@hotmail.com>
2020-12-26 18:32:52 -0300

    deploy

--- src/server.js
+++ src/server.js
@@ -15,7 +15,12 @@ dotenv.config();
 const __dirname = path.resolve();
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  },
+});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
